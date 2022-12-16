a02f207657c08e7fd0752efaeaff873ee270d7e0
Katharina Hayer <HAYERK@EMAIL.CHOP.EDU>
2022-12-16 11:22:36 -0500

    let's try again

--- workflow/scripts/merge_STAR_junctions.py
+++ workflow/scripts/merge_STAR_junctions.py
@@ -3,20 +3,22 @@ import sys
 import pandas as pd
 from snakemake.io import glob_wildcards
 
-log_file = open(snakemake.log[0], 'w')
+#log_file = open(snakemake.log[0], 'w')
 
-#args = sys.argv
-#args[1] should hold the directory with junction files
-dir_junc = snakemake.params[0]
-log_file.write(dir_junc + "\n")
+args = sys.argv
+#args[0] should hold the directory with junction files
+dir_junc = args[0]
+print(dir_junc + "\n")
+#log_file.write(dir_junc + "\n")
 
 #args[2] should name and directory of the output file
-outfile = snakemake.output[0]
-log_file.write(outfile + "\n")
+outfile = args[1]
+print(outfile + "\n")
+#log_file.write(outfile + "\n")
 #all_junc_files = os.listdir(dir_junc)
 #all_junc_files = [name for name in all_junc_files if '_SJ.out.tab' in name]
 #len(all_junc_files)
-all_junc_files = snakemake.input
+all_junc_files = args[2:]
 all_junc_files = [sub.replace('results/mapped/', '') for sub in snakemake.input]
 junctions = pd.read_csv(dir_junc + all_junc_files[0], sep = "\t", header = None,
 	usecols = [0,1,2,3,5,6,7,8],
@@ -52,7 +54,7 @@ for f in all_junc_files:
     c_junc_df = c_junc_df.loc[:,("chr","start","stop","unique","norm","sample_name")]
     junc_df  = pd.concat([junc_df, c_junc_df])
 
-log_file.write("ALL GOOD\n")
+print("ALL GOOD\n")
 
 #junc_df.to_csv(outfile + "_kat_version.csv")
 
@@ -67,7 +69,7 @@ log_file.write("ALL GOOD\n")
 #24,chr1,17526,188049,10,0.3071692504412947,SLN2420
 
 pivot_df = junc_df.pivot_table(index = ["chr","start", "stop"], columns="sample_name", values = "norm").fillna(0)
-log_file.write("Pivot done\n")
+print("Pivot done\n")
 
 pivot_df.to_csv(outfile)
-log_file.close()
+#log_file.close()
